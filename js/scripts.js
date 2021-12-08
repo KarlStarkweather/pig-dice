d253c8af105d900c45dc820a90cb50a234ebc7f0
KarlStarkweather <stark13@usa.net>
2021-12-08 13:14:28 -0800

    debug updating of total scores

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@
 function Player(name) {
   this.name = name;
   this.currentRoll = 0;
-  this.turnScore = 0;
+  this.roundScore = 0;
   this.position = 0;
 }
 
 Player.prototype.rollDice = function(game1) {
   const roll =  Math.floor((Math.random() * 6) + 1);
   this.currentRoll = roll;
-  this.turnScore += roll;
+  this.roundScore += roll;
   if (roll === 1) {
-    this.turnScore = 0;
+    this.roundScore = 0;
     this.hold(game1);
   };
 }
@@ -28,25 +28,25 @@ function Game(player1, player2) {
   this.currentPlayer = player1;
 }
 
-Game.prototype.playForComputer = function() {
-  this.player2.rollDice();
+Game.prototype.playForComputer = function(game1) { // computer takes 2 rolls
+  this.player2.rollDice(game1);
   if (this.player2.currentRoll > 1) {
-    this.player2.rollDice();
+    this.player2.rollDice(game1);
   } if (this.player2.currentRoll > 1) {
-    this.player2.hold();
+    this.player2.hold(game1);
   }
 }
 
-Player.prototype.hold = function(thisGame) {
+Player.prototype.hold = function(game1) {
   if (this.position === 1) {
-    thisGame.player1Total = this.turnScore;
-    thisGame.currentPlayer = thisGame.player2;
-    thisGame.playForComputer();
+    game1.player1Total += this.roundScore;
+    game1.currentPlayer = game1.player2;
+    game1.playForComputer(game1);
   } else {
-    thisGame.player2Total += this.turnScore;
-    thisGame.currentPlayer = thisGame.player1;
+    game1.player2Total += this.roundScore;
+    game1.currentPlayer = game1.player1;
   };
-  this.turnScore = 0;
+  this.roundScore = 0;
 }
 
 
@@ -54,9 +54,10 @@ Player.prototype.hold = function(thisGame) {
 $(document).ready(function() {
   $("#begin").click(function() {
     $("#playerName").show();
+    $("#nameLabel").show();
     $("#begin").hide();
-    $(".hiddenStuff").show();
-    $("#roll").hide();
+    // $(".hiddenStuff").show();
+    // $("#roll").hide();
     $("#playerName").change(function() {
       $("#roll").show();
       $("#playerName").hide();
@@ -66,21 +67,24 @@ $(document).ready(function() {
       let game1 = new Game(player1,player2);
       $("#player1-name").text("Player One: " + player1.name);
       $("#player2-name").text("Player Two: " + player2.name);
-      console.log(game1);
       $("#roll").click(function() {
         player1.rollDice(game1);
-        console.log("Current Roll: " + player1.currentRoll);
-        console.log("Score this round: " + player1.turnScore);
-        console.log("Game total: " + game1.player1Total);
         $("#player1-roll").text(player1.currentRoll);
         $("#player2-roll").text(player2.currentRoll);
-        $("#player1-score").text(player1.turnScore);
-        $("#player2-score").text(player2.turnScore);
+        $("#player1-score").text(player1.roundScore);
+        $("#player2-score").text(player2.roundScore);
         $("#player1-total").text(game1.player1Total);
         $("#player2-total").text(game1.player2Total);
-        $("#hold").click(function() {
-          player1.hold();
+        
         });
+      $("#hold").click(function() {
+        player1.hold(game1);
+        $("#player1-roll").text(player1.currentRoll);
+        $("#player2-roll").text(player2.currentRoll);
+        $("#player1-score").text(player1.roundScore);
+        $("#player2-score").text(player2.roundScore);
+        $("#player1-total").text(game1.player1Total);
+        $("#player2-total").text(game1.player2Total);
       });
     })
   })
